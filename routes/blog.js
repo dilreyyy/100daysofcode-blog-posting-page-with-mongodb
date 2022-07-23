70722dfb2f84aa27e610705e3371d007c689a8ac
Its Ya Boi Del Rey <delreeyyy@gmail.com>
2022-07-23 16:55:41 +0800

    Insert Blog Post Data

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('../database/database');
 const router = express.Router();
+const ObjectId = require('mongodb').ObjectId;
 
 router.get('/', function(req, res) {
   res.redirect('/posts');
@@ -14,6 +15,26 @@ router.get('/new-post', async function(req, res) {
   const authors = await db.getDb().collection('blog_authors').find().toArray();
   // console.log(authors);
   res.render('create-post', {authors: authors});
+});
+
+//INSERT DATA
+router.post('/posts', async function(req, res){
+  const authorId = new ObjectId(req.body.author);
+  const result = await db.getDb().collection('blog_authors').findOne(authorId);
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    content: req.body.content,
+    date: new Date(),
+    author: {
+      authorId: authorId,
+      author_name: result.author_name,
+      author_email: result.author_email
+    }
+  }
+
+  await db.getDb().collection('blog_posts').insertOne(newPost);
+  res.redirect('/posts');
 });
 
 module.exports = router;
